5f221ab56b7b222048cd3ddc4f857319b9705198
@saadaoui-salah <salahsaadaoui8@gmail.com>
2021-05-17 01:36:03 -0700

    create qeue node

---
+++ qeue.py
@@ -0,0 +1,22 @@
+class Node:
+    def __init__(self, data):
+        self.next = None
+        self.data = data
+
+class Stack:
+    def __init__(self):
+        self.head = None
+
+    def push(self, data):
+        if self.head is None:
+            self.head = Node(data)
+            return self.head
+        new_node = Node(data)
+        new_node.next = self.head
+        self.head = new_node
+
+    def pop(self):
+        if self.head is not None:
+            popped = self.head.data
+            self.head = self.head.next
+            return popped
