9079848309bf20108efe9fb0d6ed90f659b038e8
@saadaoui-salah <salahsaadaoui8@gmail.com>
2021-05-18 10:03:45 -0700

    start emplementing tree

---
+++ Tree.py
@@ -0,0 +1,11 @@
+class Node:
+    def __init__(self, root, left, right):
+        self.left = left
+        self.right = right
+
+class Tree:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, data):
+        
